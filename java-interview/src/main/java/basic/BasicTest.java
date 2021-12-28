fbe5a68e84564cfe809b356cedec926115ada444
Lounes.Brahimi <lounes.brahimi@etu.sorbonne-universite.fr>
2021-12-28 10:28:22 +0100

    Basic test Done

--- java-interview/src/main/java/basic/BasicTest.java
+++ java-interview/src/main/java/basic/BasicTest.java
@@ -7,11 +7,35 @@ import io.vavr.control.Option;
  */
 public class BasicTest {
 
-  /**
-   * return i ^ n for positive Integer, None otherwise
-   * alse return None in case of errors
-   */
-  public static Option<Integer> power(Integer i, Integer n) {
-    return Option.none();
-  }
+	  /**
+	   * return i ^ n for positive Integer, None otherwise
+	   * alse return None in case of errors
+	   */
+	  public static Option<Integer> power(Integer i, Integer n) {
+      try{
+        // check that they are positive
+        if (i >= 0 && n >= 0){
+          // i^0 = 1
+          if (n == 0){
+            return Option.of(1);
+            //0^n = 0
+          } else if (i == 0 && n >0){
+            return Option.of(0);
+          } else {
+            // Do a recursive calculation
+            Option<Integer> result = power(i, n-1);
+            if (result.isEmpty()) {
+              return Option.none();
+            } else {
+              return Option.of(i * result.get());   
+            } 
+          }
+        } else {
+          return Option.none();
+        }
+      }
+      catch(StackOverflowError e){
+          return Option.none();
+      }
+    }
 }
